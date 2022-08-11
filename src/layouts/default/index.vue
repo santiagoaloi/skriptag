a216659f48f38f0bb1ed3b07f207d89df7b27e93
Santiago Aloi <santiagoaloi@gmail.com>
2022-08-11 20:37:41 +0200

    Moved layout view sfc inside layout index

--- src/layouts/default/index.vue
+++ src/layouts/default/index.vue
@@ -1,10 +1,21 @@
 <template>
   <v-app>
-    <default-bar />
-    <default-drawer v-if="$route.meta.requiresAuth" />
-    <default-view />
+    <app-bar />
+
+    <app-drawer v-if="$route.meta.requiresAuth" />
+
+    <v-main>
+      <v-fade-transition :duration="130" mode="out-in">
+        <keep-alive>
+          <router-view />
+        </keep-alive>
+      </v-fade-transition>
+    </v-main>
+
     <route-loader />
+
     <base-back-to-top></base-back-to-top>
+
     <base-snackbar />
   </v-app>
 </template>
@@ -12,10 +23,9 @@
   export default {
     name: 'DefaultLayout',
     components: {
-      DefaultBar: () => import(/* webpackChunkName:  'default-layout'  */ './AppBar'),
-      DefaultDrawer: () => import(/* webpackChunkName:  'default-layout'  */ './Drawer'),
-      DefaultView: () => import(/* webpackChunkName:  'default-view'  */ './View'),
-      RouteLoader: () => import(/* webpackChunkName:  'route-layout'  */ './Loader'),
+      AppBar: () => import(/* webpackChunkName:  'default-layout'  */ './AppBar'),
+      AppDrawer: () => import(/* webpackChunkName:  'default-layout'  */ './Drawer'),
+      RouteLoader: () => import(/* webpackChunkName:  'default-layout'  */ './Loader'),
     },
   };
 </script>
